4656a7cbbbb534bfa96073dfecafc0cbc167607b
Aleksey Kulikov <skinny.mind@gmail.com>
2021-05-26 14:40:43 +0300

    feat: add message above qr code

--- bin/main.dart
+++ bin/main.dart
@@ -1,8 +1,11 @@
 import 'package:qrcp/qr/qr.dart';
 
 void main(List args) {
-  final serverIP = '192.168.0.1';
+  final serverIP = '192.168.0.1:1337';
   final qr = generateQR('http://$serverIP/${args[0]}/qWe3');
 
+  print('Scan the following URL with QR scanner to start the file transfer:');
+  print('http://$serverIP/${args[0]}/qWe3');
+  print('');
   print(qr);
 }
