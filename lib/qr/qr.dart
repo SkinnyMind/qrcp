504c56b2d72f256d54f9707e6cdafcf8fcef031b
Aleksey Kulikov <skinny.mind@gmail.com>
2023-06-30 19:55:42 +0300

    chore: bump version of qr package

--- lib/qr/qr.dart
+++ lib/qr/qr.dart
@@ -4,28 +4,28 @@ String generateQR(String data) {
   final qrCode = QrCode.fromData(
     data: data,
     errorCorrectLevel: QrErrorCorrectLevel.L,
-  )..make();
-
+  );
+  final qrImage = QrImage(qrCode);
   final result = StringBuffer();
 
   // Draws dark blocks in terminal with light background.
   // Swap ternary result if your terminal have dark bacgkround.
   // Most of the qr scanners won't have any problems reading qr either way.
-  for (var y = 0; y < qrCode.moduleCount; y += 2) {
+  for (var y = 0; y < qrImage.moduleCount; y += 2) {
     // even number of rows in qr
-    if (y != qrCode.moduleCount - 1) {
-      for (var x = 0; x < qrCode.moduleCount; x++) {
-        if (qrCode.isDark(x, y) == qrCode.isDark(x, y + 1)) {
-          qrCode.isDark(x, y) ? result.write('█') : result.write(' ');
+    if (y != qrImage.moduleCount - 1) {
+      for (var x = 0; x < qrImage.moduleCount; x++) {
+        if (qrImage.isDark(x, y) == qrImage.isDark(x, y + 1)) {
+          qrImage.isDark(x, y) ? result.write('█') : result.write(' ');
         } else {
-          qrCode.isDark(x, y) ? result.write('▀') : result.write('▄');
+          qrImage.isDark(x, y) ? result.write('▀') : result.write('▄');
         }
       }
       result.write('\n');
     } else {
       // special treatment for last row in odd numbered qr
-      for (var x = 0; x < qrCode.moduleCount; x++) {
-        qrCode.isDark(x, y) ? result.write('▀') : result.write(' ');
+      for (var x = 0; x < qrImage.moduleCount; x++) {
+        qrImage.isDark(x, y) ? result.write('▀') : result.write(' ');
       }
     }
   }
